66468953781e140f4e8fbba8215bd0d39a842659
Dreamaker <lj270889@hotmail.com>
2024-05-13 23:27:45 -0300

    PSS (Split)
    Physical / Special / Status (Split)

--- src/pokemon.c
+++ src/pokemon.c
@@ -2460,9 +2460,7 @@ s32 CalculateBaseDamage(struct BattlePokemon *attacker, struct BattlePokemon *de
         if (attackerHoldEffect == sHoldEffectToType[i][0]
             && type == sHoldEffectToType[i][1])
         {
-            if (IS_TYPE_PHYSICAL(type))
                 attack = (attack * (attackerHoldEffectParam + 100)) / 100;
-            else
                 spAttack = (spAttack * (attackerHoldEffectParam + 100)) / 100;
             break;
         }
@@ -2514,7 +2512,7 @@ s32 CalculateBaseDamage(struct BattlePokemon *attacker, struct BattlePokemon *de
     if (gBattleMoves[gCurrentMove].effect == EFFECT_EXPLOSION)
         defense /= 2;
 
-    if (IS_TYPE_PHYSICAL(type))
+    if (IS_TYPE_PHYSICAL(gBattleMoves[move]))
     {
         if (gCritMultiplier == 2)
         {
@@ -2569,7 +2567,7 @@ s32 CalculateBaseDamage(struct BattlePokemon *attacker, struct BattlePokemon *de
     if (type == TYPE_MYSTERY)
         damage = 0; // is ??? type. does 0 damage.
 
-    if (IS_TYPE_SPECIAL(type))
+    if (IS_TYPE_SPECIAL(gBattleMoves[move]))
     {
         if (gCritMultiplier == 2)
         {
